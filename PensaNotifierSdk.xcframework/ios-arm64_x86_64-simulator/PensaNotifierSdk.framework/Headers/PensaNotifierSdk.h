61de3c20c6d77107cf27de7919699309224b98e8
ozgurbaykalinitial <obaykal@initialcode.com.tr>
2025-08-14 19:06:17 +0300

    Initial commit

---
+++ PensaNotifierSdk.xcframework/ios-arm64_x86_64-simulator/PensaNotifierSdk.framework/Headers/PensaNotifierSdk.h
@@ -0,0 +1,19 @@
+//
+//  PensaNotifierSdk.h
+//  PensaNotifierSdk
+//
+// Copyright (c) 2025 by Pensa Systems, Inc. -- All rights reserved
+// Confidential and Proprietary
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PensaNotifierSdk.
+FOUNDATION_EXPORT double PensaNotifierSdkVersionNumber;
+
+//! Project version string for PensaNotifierSdk.
+FOUNDATION_EXPORT const unsigned char PensaNotifierSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PensaNotifierSdk/PublicHeader.h>
+
+
